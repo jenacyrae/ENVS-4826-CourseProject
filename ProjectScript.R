cdc7ecee028760053124fe8224ac99feff862d1f
jenacyrae <71839796+jenacyrae@users.noreply.github.com>
2020-11-21 18:59:59 -0400

    Data cleaning
    Removing unwanted columns from the usaram_summary and usaram_siteinfo datasets to create new dataframes

--- ProjectScript.R
+++ ProjectScript.R
@@ -8,4 +8,5 @@ usaram_siteinfo <- read.csv("data/nwca2011_siteinfo copy.csv")
 
 # Data cleaning
 
-wetlanddata <- usaram_summary[-c(1,6,8:9,11,15:23)]
+wetlanddata <- usaram_summary[-c(1,6,8:9,11,15:23,28)]
+wetlandlocationdata <- usaram_siteinfo[c(2,9,10,14,28)]
