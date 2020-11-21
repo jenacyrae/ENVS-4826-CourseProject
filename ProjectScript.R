1774b6a06bfc8c41a13c6b8d12d1d148460567bb
jenacyrae <71839796+jenacyrae@users.noreply.github.com>
2020-11-21 19:23:14 -0400

    Data joining
    Joining our two datasets by their UID

--- ProjectScript.R
+++ ProjectScript.R
@@ -8,5 +8,10 @@ usaram_siteinfo <- read.csv("data/nwca2011_siteinfo copy.csv")
 
 # Data cleaning
 
-wetlanddata <- usaram_summary[-c(1,6,8:9,11,15:23,28)]
+wetlandsurveydata <- usaram_summary[-c(1,4:6,8:9,11,15:23,28)]
 wetlandlocationdata <- usaram_siteinfo[c(2,9,10,14,28)]
+
+#Data joining 
+
+wetlanddata <- inner_join(wetlandsurveydata,wetlandlocationdata, by = c("UID"))
+
