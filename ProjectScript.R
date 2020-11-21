ecafc0862a12b61e6a9d58c4aac94205dc5cf381
jenacyrae <71839796+jenacyrae@users.noreply.github.com>
2020-11-21 18:30:44 -0400

    Data cleaning
    Removing unwanted columns from the usaram_summary dataset

--- ProjectScript.R
+++ ProjectScript.R
@@ -6,3 +6,6 @@ usaram_summary <- read.csv("data/nwca2011_usaram_summary.csv")
 usaram_attributes <- read.csv("data/nwca2011_usaram_attributes.csv")
 usaram_siteinfo <- read.csv("data/nwca2011_siteinfo copy.csv")
 
+# Data cleaning
+
+wetlanddata <- usaram_summary[-c(1,6,8:9,11,15:23)]
