78e68dcae97c9babfed8944821953f1dfedbd18f
jenacyrae <71839796+jenacyrae@users.noreply.github.com>
2020-11-21 19:59:59 -0400

    Further data cleaning
    Renaming of data columns

--- ProjectScript.R
+++ ProjectScript.R
@@ -11,7 +11,18 @@ usaram_siteinfo <- read.csv("data/nwca2011_siteinfo copy.csv")
 wetlandsurveydata <- usaram_summary[-c(1,4:6,8:9,11,15:23,28)]
 wetlandlocationdata <- usaram_siteinfo[c(2,9,10,14,28)]
 
-#Data joining 
+# Data joining 
 
 wetlanddata <- inner_join(wetlandsurveydata,wetlandlocationdata, by = c("UID"))
+
+# Further data cleaning
+
+library(tidyverse)
+library(janitor)
+
+wetlanddata <- wetlanddata %>% 
+  janitor::clean_names()
+
+wetlanddata <- rename(wetlanddata,"UID"="uid","date"="date_col","agri_severity"="agr_severity_m3","habitat_severity"="habitat_severity_m3","hydro_severity"="hyd_stressors_severity_m3","landuse_severity"="landuse_severity_m3","total_invasive_cover"="total_invasive_cover_m11","grazing_severity"="excessive_grazing_severity_m12","fire_severity"="fire_severity_m12","human_use_severity"="human_use_severity_m12","latitude"="analysis_lat","longitude"="analysis_lon","region"="coe_region","major_river_basin"="maj_river_basin")
+
 
