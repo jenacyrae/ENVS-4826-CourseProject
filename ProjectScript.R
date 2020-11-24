aecf1f81b1254e592c113d01014b4db67f6c47ff
jenacyrae <71839796+jenacyrae@users.noreply.github.com>
2020-11-24 17:05:46 -0400

    Data cleaning
    Removing duplicate site visits

--- ProjectScript.R
+++ ProjectScript.R
@@ -8,10 +8,12 @@ usaram_siteinfo <- read.csv("data/nwca2011_siteinfo copy.csv")
 
 # Data cleaning
 
-wetlandsurveydata <- usaram_summary[-c(1,4:6,8:9,11,15:23,28)]
-wetlandlocationdata <- usaram_siteinfo[c(2,9,10,14,28)]
+wetlandsurveydata <- usaram_summary[-c(1,3,4,6,8:9,11,15:23,28)]
+wetlandlocationdata <- usaram_siteinfo[c(2,9,10,14)]
 
 # Data joining 
+
+library(dplyr)
 
 wetlanddata <- inner_join(wetlandsurveydata,wetlandlocationdata, by = c("UID"))
 
@@ -23,6 +25,10 @@ library(janitor)
 wetlanddata <- wetlanddata %>% 
   janitor::clean_names()
 
-wetlanddata <- rename(wetlanddata,"UID"="uid","date"="date_col","agri_severity"="agr_severity_m3","habitat_severity"="habitat_severity_m3","hydro_severity"="hyd_stressors_severity_m3","landuse_severity"="landuse_severity_m3","total_invasive_cover"="total_invasive_cover_m11","grazing_severity"="excessive_grazing_severity_m12","fire_severity"="fire_severity_m12","human_use_severity"="human_use_severity_m12","latitude"="analysis_lat","longitude"="analysis_lon","region"="coe_region","major_river_basin"="maj_river_basin")
+wetlanddata <- rename(wetlanddata,"UID"="uid","agri_severity"="agr_severity_m3","habitat_severity"="habitat_severity_m3","hydro_severity"="hyd_stressors_severity_m3","landuse_severity"="landuse_severity_m3","total_invasive_cover"="total_invasive_cover_m11","grazing_severity"="excessive_grazing_severity_m12","fire_severity"="fire_severity_m12","human_use_severity"="human_use_severity_m12","latitude"="analysis_lat","longitude"="analysis_lon","region"="coe_region")
+
+# Removing duplicate site visits
+
+wetlanddata <- wetlanddata[wetlanddata$visit_no != 2, ]
 
 
